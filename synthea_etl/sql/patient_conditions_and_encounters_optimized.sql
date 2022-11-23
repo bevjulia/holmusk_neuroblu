44d1f0c82e7f014294cccde79b04a9b8bb7a59da
Beverly Chua <bevjulia@gmail.com>
2022-11-23 22:16:08 +0800

    minor formatting for sql

--- synthea_etl/sql/patient_conditions_and_encounters_optimized.sql
+++ synthea_etl/sql/patient_conditions_and_encounters_optimized.sql
@@ -3,7 +3,10 @@ select *
 from synthea_native.patients p
 inner join synthea_native.conditions c
 on c.patient = p.id
-inner join (select distinct * from synthea_native.encounters) e
+inner join (
+    select distinct * 
+    from synthea_native.encounters
+    ) e
 on e.id = c.encounter
 and e.patient = c.patient
 where 1=1
